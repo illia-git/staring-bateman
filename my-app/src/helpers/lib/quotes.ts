71d616ea274489605b871dc9e832c96c065a60e4
illia-git <ichykurov@gmail.com>
2022-12-23 23:35:22 +0100

    add quotes lib

---
+++ my-app/src/helpers/lib/quotes.ts
@@ -0,0 +1,53 @@
+interface IQuote {
+	quote : string;
+}
+
+interface IQuotes extends Array<IQuote> {
+}
+
+const quotes : IQuotes = [
+	{
+		quote : 'I Want To Stab You To Death And Play Around With Your Blood.'
+	},
+	{
+		quote : 'There Is An Idea Of Patrick Bateman.'
+	},
+	{
+		quote : 'You Like Huey Lewis And The News?'
+	},
+	{
+		quote : 'Let\'s See Paul Allen\'s Card.'
+	},
+	{
+		quote : 'Hey Paul! Try Getting A Reservation At Dorsia Now!'
+	},
+	{
+		quote : 'It Even Has A Watermark.'
+	},
+	{
+		quote : 'I Need To Engage In Homicidal Behavior on A Massive Scale.'
+	},
+	{
+		quote : 'I\'ve Killed A Lot Of People.'
+	},
+	{
+		quote : 'I’m Into Murders And Executions Mostly.'
+	},
+	{
+		quote : 'I Think My Mask Of Sanity Is About To Slip.'
+	},
+	{
+		quote : 'My Pain Is Constant And Sharp, And I Do Not Wish For A Better World For Anyone.'
+	},
+	{
+		quote : 'I Have To Return Some Videotapes.'
+	},
+	{
+		quote : 'This Confession Has Meant Nothing.'
+	},
+	{
+		quote : 'I Simply Am Not There.'
+	}
+];
+
+export default quotes;

f6b99da0a6e3359fd2f843f254651457cda8ed10
illia-git <ichykurov@gmail.com>
2022-12-29 01:42:48 +0100

    add logic to BackgroundImage.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import BackgroundImage from './components/BackgroundImage';
+
 function App() : JSX.Element {
     return (
         <div className="App">
+            <BackgroundImage type="eng"/>
         </div>
     );
 }
---
+++ my-app/src/components/BackgroundImage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import bateman_eng from '../assets/images/bateman_eng.gif';
+import bateman_arab from '../assets/images/bateman_arab.gif';
+
+interface IBackgroundImageProps {
+	type : 'eng' | 'arab';
+}
+
+const BackgroundImage : React.FunctionComponent<IBackgroundImageProps> = ({ type }) : JSX.Element => {
+	const GIF = type === 'eng' ? bateman_eng : bateman_arab;
+	return (
+		<div
+			style={ {
+				backgroundImage : `url(${ GIF })`,
+				backgroundSize : 'cover',
+				height : '100vh',
+				width : '100vw'
+			} }
+		/>
+	);
+};
+
+export default BackgroundImage;
